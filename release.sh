190940fe51d9b7aff0a94eb5b7e8fad888223182
Jun Koyama <koyama@tagbangers.co.jp>
2019-03-20 19:42:09 +0900

    タグバンのnexusで運用できるよう設定を追加

--- release.sh
+++ release.sh
@@ -1,17 +1,9 @@
-#!/bin/bash
-git pull
-# update changelog
-cd ./src
-node ../node_modules/standard-version/bin/cli.js --infile ../CHANGELOG.md
-cd ..
-
 # build lib
 yarn run build
 
-# push tags
-git push --follow-tags origin master
-
 # push to npm
 cp README.md ./dist
+cp .src/package.json ./dist
+cp .npmrc ./dist
 cd ./dist
-yarn publish --access=public
+npm publish
